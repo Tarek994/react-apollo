1a723405d9a293c46b05efd1437b254996589096
Tarek994 <ghali.tarek.123@hotmail.com>
2023-08-03 22:47:08 +0300

    feat(posts.tsx): add the PostGrid component depend on Interfaces porps

---
+++ src/components/posts-grid/posts-grid.component.tsx
@@ -0,0 +1,8 @@
+import { Post } from "../../common/interfaces/post.interfaces"
+
+const PostGrid: React.FC<{posts: Post}> = () => {
+    return (
+        <div>Post Grid</div>
+    )
+}
+export default PostGrid
