a7a2a16e3f44cab2eb60abf96eeafa631d79b240
Tarek994 <ghali.tarek.123@hotmail.com>
2023-08-05 23:21:41 +0300

    feat(home.tsx): add the post array details

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -3,8 +3,14 @@ import PostGrid from "../../components/posts-grid/posts-grid.component"
 
 
 const Home: React.FC = () => {
-    return(
-        <div className="home"> Home</div>
+    const posts: Post[] = [
+        {
+            id: '1',
+            title: "Post one",
+            body: "Body of my post"
+        }
+    ]
+
     )
 }
 
