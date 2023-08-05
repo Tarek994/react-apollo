7e8842d27a959b819bd1f462d8e6c5b43266b8fa
Tarek994 <ghali.tarek.123@hotmail.com>
2023-08-05 23:17:23 +0300

    feat(home.tsx): import Post and PostGrid to the home page

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -1,3 +1,7 @@
+import { Post } from "../../common/interfaces/post.interfaces"
+import PostGrid from "../../components/posts-grid/posts-grid.component"
+
+
 const Home: React.FC = () => {
     return(
         <div className="home"> Home</div>
