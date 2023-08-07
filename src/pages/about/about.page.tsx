f9dd60955ccbd1aabcdaf1f2325aae2a10bc30c4
Tarek994 <ghali.tarek.123@hotmail.com>
2023-08-07 23:39:36 +0300

    feat(about.tsx): import the PostsForm to the about component

--- src/pages/about/about.page.tsx
+++ src/pages/about/about.page.tsx
@@ -1,7 +1,11 @@
+import PostsForm from "../../components/posts/posts-form/posts-form.component"
 
 const About: React.FC = () => {
-    return(
-        <div className="about">About</div>
+    return (
+        <div className="about">
+            <PostsForm />
+
+        </div>
     )
 }
 
