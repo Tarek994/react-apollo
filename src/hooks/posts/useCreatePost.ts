a269f3a04663ab9d58cb819922724e6a672039af
Tarek994 <ghali.tarek.123@hotmail.com>
2023-08-09 18:26:05 +0300

    feat(useCreatePost.ts): add the useCreatePost fun

--- src/hooks/posts/useCreatePost.ts
+++ src/hooks/posts/useCreatePost.ts
@@ -20,3 +20,29 @@ const CREATE_POST = gql`
     }
 
 `
+export const useCreatePost = (): ((
+    createPostInput: CreatePostInput,
+) => any) => {
+    const [createPost] = useMutation(CREATE_POST, {
+        update(cache, { data: { createPost } }) {
+            cache.modify({
+                fields: {
+                    posts(existingPosts = []) {
+                        const newPostRef = cache.writeFragment({
+                            data: createPost,
+                            fragment: gql`
+                            fragment NewPost on Post {
+                                id,
+                                title,
+                                body
+                            }
+                            `
+                        });
+                        return [...existingPosts, newPostRef]
+                    }
+                }
+            })
+        }
+    });
+    return createPost
+}
