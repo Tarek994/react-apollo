514e1129d2dda837011d4b2f0ad44a35a7381399
Tarek994 <ghali.tarek.123@hotmail.com>
2023-08-17 23:31:30 +0300

    chore(home.tsx): import the home.css file

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -1,6 +1,6 @@
 import PostGrid from "../../components/posts/posts-grid/posts-grid.component"
 import { useGetPosts } from "../../hooks/posts/useGetPosts"
-
+import "./home.page.css"
 
 const Home: React.FC = () => {
     const posts = useGetPosts();
