0524e3ffdc62c9843a64b4da221fa4896cb64cbe
Tarek994 <ghali.tarek.123@hotmail.com>
2023-08-20 18:36:18 +0300

    feat(home.tsx): add the image with the postGrid in the home page

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -1,11 +1,15 @@
 import PostGrid from "../../components/posts/posts-grid/posts-grid.component"
 import { useGetPosts } from "../../hooks/posts/useGetPosts"
 import "./home.page.css"
+import home from "./assets/home.svg"
 
 const Home: React.FC = () => {
     const posts = useGetPosts();
     return (
         <div className="home">
+            <div>
+                <img src={home} alt="" />
+            </div>
             <PostGrid posts={posts || []} />
         </div>
     )
